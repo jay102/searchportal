a4dabc5b5a211e0c00c940e3f0c550ddd876183b
James <jordunjames@gmail.com>
2020-04-11 18:31:10 +0100

    date range picker fix

--- src/components/Modals/DateRangepicker.jsx
+++ src/components/Modals/DateRangepicker.jsx
@@ -2,21 +2,28 @@ import React, {useState} from 'react';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
-import { Calendar } from 'react-date-range';
+import { DateRange } from 'react-date-range';
+
 
 const Picker = ({className, visible, toggle}) => {
-    const [date, setDate] = useState(new Date())
-    const handleSelect = (date) => setDate(date);
+    const [state, setState] = useState([
+        {
+          startDate: new Date(),
+          endDate: null,
+          key: 'selection'
+        }
+      ]);
 
     return (
         <Modal isOpen={visible} toggle={toggle} className={className}>
         <ModalHeader toggle={toggle}>Range</ModalHeader>
         <ModalBody>
-        <Calendar
-        date={date}
-        direction="horizontal"
-        onChange={handleSelect}
-      />
+        <DateRange
+            editableDateInputs={true}
+            onChange={item => setState([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={state}
+            />
         
         </ModalBody>
         </Modal>
